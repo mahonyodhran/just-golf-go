dacd6848194163ea970d45c80f90d3b425ddb838
Odhran Mahony <mahonyodhran@gmail.com>
2024-01-26 22:03:18 +0000

    Added driver file

---
+++ main.go
@@ -0,0 +1,14 @@
+package main
+
+import (
+	"log"
+	"net/http"
+)
+
+func main() {
+	InitDB()
+	defer db.Close()
+	InitializeApp()
+	log.Println("App starting...")
+	log.Fatal(http.ListenAndServe(":8008", nil))
+}
