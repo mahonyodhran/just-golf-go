5474000decf523f76ae5cee12d89402da7d1c20a
Odhran Mahony <mahonyodhran@gmail.com>
2024-01-26 22:49:34 +0000

    added convoluted scorecards handler

--- handlers.go
+++ handlers.go
@@ -4,17 +4,56 @@ import (
 	"fmt"
 	"html/template"
 	"net/http"
+	"os"
 	"strconv"
 )
+
+func inc(i int) int {
+	return i + 1
+}
 
 func InitializeApp() {
 	http.HandleFunc("/", IndexHandler)
 	http.HandleFunc("/scorecard", ScorecardHandler)
+	http.HandleFunc("/scorecards", ScorecardsHandler)
 }
 
 func IndexHandler(w http.ResponseWriter, r *http.Request) {
 	tmpl := template.Must(template.ParseFiles("templates/index.html"))
 	tmpl.Execute(w, nil)
+}
+
+func ScorecardsHandler(w http.ResponseWriter, r *http.Request) {
+	scorecards, err := getScorecards()
+	if err != nil {
+		fmt.Println(err) // TODO
+	}
+
+	// TODO - Clean up this, very awkwarad for some reason
+	// Read the template file
+	templateFile, err := os.ReadFile("templates/scorecards.html")
+	if err != nil {
+		fmt.Println("Error reading template file:", err)
+		return
+	}
+
+	// Convert the file content to a string
+	templateContent := string(templateFile)
+
+	// Parse the HTML template
+	tmpl := template.New("scorecards").Funcs(template.FuncMap{"inc": inc})
+	tmpl, err = tmpl.Parse(templateContent)
+	if err != nil {
+		fmt.Println("Error parsing template:", err)
+		return
+	}
+
+	fmt.Println("Parsed Template:", tmpl.DefinedTemplates())
+
+	err = tmpl.Execute(w, scorecards)
+	if err != nil {
+		fmt.Println(err) // TODO: Handle the error appropriately
+	}
 }
 
 func ScorecardHandler(w http.ResponseWriter, r *http.Request) {
